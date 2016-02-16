53e3359306d49cc60e2b4922c00398f53e6e38f9
bobby yawn <bobby.yawn@carcarepeople.com>
2016-02-16 16:27:06 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,43 @@
+var express =   require("express");
+var bodyParser  = require('body-parser');
+var multer  =   require('multer');
+var app         =   express();
+var storage =   multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, './audio');
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.fieldname + '-' + Date.now());
+  }
+});
+var upload = multer({ storage : storage}).single('userPhoto');
+
+//app.use(serveStatic(__dirname + '/audio/'));
+app.get('/',function(req,res){
+      res.sendFile(__dirname + "/index.html");
+});
+
+app.get('/test.jpg', function (req, res) {
+  //console.log(req.params);
+  res.sendFile(__dirname + '/test.jpg');
+});
+app.get('/audio/:thing', function (req, res) {
+  //console.log(req.params.thing);
+  res.sendFile(__dirname + '/audio/' + req.params.thing);
+});
+
+app.use( bodyParser.json() );
+app.use( bodyParser.urlencoded( { extended: true } ) );
+
+app.post('/api/photo',function(req,res){
+    upload(req,res,function(err) {
+        if(err) {
+            return res.end("Error uploading file.");
+        }
+        res.end("File is uploaded");
+    });
+});
+
+app.listen(3000,function(){
+    console.log("Working on port 3000");
+});
